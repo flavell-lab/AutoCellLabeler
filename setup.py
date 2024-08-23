00d31a7cb2d3a46702e49b50e770df4e27d88ec2
Adam Atanas <adamatanas@gmail.com>
2024-08-23 00:11:01 +0000

    Update version to v1.0.0. Add AutoCellLabeler_freely_moving notebook

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="autolabel",
-    version="0.2.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=["numpy", "pynrrd", "h5py", "tqdm", "openpyxl", "pandas", "matplotlib"],
     author="Adam Atanas",
     author_email="adamatanas@gmail.com",
     description="A Python package to automate NeuroPAL labeling via interfacing with a 3D-UNet.",
     license="MIT",
-    url="http://github.com/flavell-lab/autolabel"
+    url="http://github.com/flavell-lab/AutoCellLabeler"
 )
 
